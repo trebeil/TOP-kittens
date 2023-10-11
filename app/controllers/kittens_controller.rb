df4ee3e8a235e72bba7f176c83367249eaf1d916
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-11 18:31:39 -0300

    Implement new and create actions and new view

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -6,4 +6,24 @@ class KittensController < ApplicationController
   def show
     @kitten = Kitten.find(params[:id])
   end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
