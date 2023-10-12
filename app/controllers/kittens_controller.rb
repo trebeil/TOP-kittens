523ef3f22236375efcebb938f8672f59b3611e92
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-11 22:59:19 -0300

    Display flash messages after creating, editing or deleting kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
+      flash[:success] = "Looks like someone new is in the house! Be welcome, #{@kitten.name}!"
       redirect_to @kitten
     else
+      flash[:error] = "Looks like someone doesn't know how to fill a simple form!"
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,14 +31,18 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash[:success] = "Changing is okay. We still love you, #{@kitten.name}!"
       redirect_to @kitten
     else
+      flash[:error] = "Can't do that, mate!"
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    Kitten.find(params[:id]).destroy
+    @kitten = Kitten.find(params[:id])
+    flash[:success] = "No more #{@kitten.name} around here. Good bye!"
+    @kitten.destroy
     redirect_to root_path
   end
 
